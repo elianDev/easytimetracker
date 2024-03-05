4122f94d5119ccb14046365a85f8784f655775df
Elian Silva <you@example.com>
2024-03-05 20:00:13 -0300

    first commit

---
+++ src/main/java/com/ett/EasyTimeTracker/EasyTimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ett.EasyTimeTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTimeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTimeTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ett/EasyTimeTracker/EasyTimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ett.EasyTimeTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyTimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
